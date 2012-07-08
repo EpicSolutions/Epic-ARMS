486cb6e0b4595a748464af3f08e8b3b3c1e668bc
Sean Malone <seanmalone.cs@gmail.com>
2012-07-07 20:47:05 -0400

    Add search for starting point

--- js/map.js
+++ js/map.js
@@ -4,13 +4,21 @@ $(document).ready(function(){
 /*************************************************************************************** 
 * Initial options & initialization
 ***************************************************************************************/
+	// Initial options
 	var myOptions = {
-		center: new google.maps.LatLng(-34.397, 150.644),
 		zoom: 8,
 		mapTypeId: google.maps.MapTypeId.ROADMAP,
 	};
 	
+	// Render map
 	$('.map').gmap(myOptions);
+	
+	// Set center at starting point
+	$('.map').gmap('search', { 'address': 'Dallas, Texas' }, function(results, status) {
+    if ( status === 'OK' ) {
+                $('.map').gmap('get', 'map').panTo(results[0].geometry.location);
+        }
+});
 
 /*************************************************************************************** 
 * Test marker
