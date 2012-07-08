9c99fb932e037aada40693f4318b33154ef1dc9f
Sean Malone <seanmalone.cs@gmail.com>
2012-07-08 18:39:13 -0400

    Add stops to html on Frontend

--- js/map.js
+++ js/map.js
@@ -1,9 +1,49 @@
 // Map
 $(document).ready(function(){
+/******************************************************************************* 
+* DOM 
+*******************************************************************************/
+$('body').append('<div class="trucks"></div>');
+$('.trucks').css('display', 'none');
 
-/*************************************************************************************** 
+/******************************************************************************* 
+* Parse JSON
+*******************************************************************************/
+$.getJSON('php/getPoints.php', function(data) {
+	$.each(data.points, function(i, val) {
+		$('.trucks').append('<div class="' + i + '"></div>');
+		
+		$.each(val, function(key, value) {
+			// Append each stop to the truck
+			$('.' + i).append('<ul id="' + i + '-' + key + 
+				'" class="' + key + '"></ul>');
+			
+			// Append details to stop
+			var list = $('#' + i + '-' + key);
+			list.append('<li class="id">'        + value.id        + '</li>');
+			list.append('<li class="time">'      + value.time      + '</li>');
+			list.append('<li class="nickName">'  + value.nickName  + '</li>');
+			list.append('<li class="cached">'    + value.cached    + '</li>');
+			list.append('<li class="tLocation">' + value.tLocation + '</li>');
+			list.append('<li class="bLevel">'    + value.bLevel    + '</li>');
+			list.append('<li class="gLevel">'    + value.gLevel    + '</li>');
+			list.append('<li class="tLevel">'    + value.tLevel    + '</li>');
+			list.append('<li class="lat">'       + value.lat       + '</li>');
+			list.append('<li class="lon">'       + value.lon       + '</li>');
+			list.append('<li class="alt">'       + value.alt       + '</li>');
+			list.append('<li class="speed">'     + value.speed     + '</li>');
+			list.append('<li class="heading">'   + value.heading   + '</li>');
+			list.append('<li class="direction">' + value.direction + '</li>');
+			
+			// Add current class to first(current) stop
+			$('.trucks').find('.i0').addClass('current');
+		}); // End val.each
+	}); // End data.points.each
+}); // End getJSON
+
+/******************************************************************************* 
 * Initial options & initialization
-***************************************************************************************/
+*******************************************************************************/
 	// Initial options
 	var myOptions = {
 		zoom: 8,
@@ -20,9 +60,9 @@ $(document).ready(function(){
         }
 });
 
-/*************************************************************************************** 
+/******************************************************************************* 
 * Test marker
-***************************************************************************************/	
+*******************************************************************************/	
 	$('.map').gmap(
 		'addMarker', 
 		{ /*id:'m_1',*/ 
@@ -32,9 +72,9 @@ $(document).ready(function(){
         $('.map').gmap('openInfoWindow', { 'content': 'TEXT_AND_HTML_IN_INFOWINDOW' }, this);
 	});
 	
-/*************************************************************************************** 
+/******************************************************************************* 
 * Add markers
-***************************************************************************************/
+*******************************************************************************/
 /*	$.getJSON( 'URL_TO_JSON', function(data) {
         $.each( data.markers, function(i, m) {
                 $('#map_canvas').gmap('addMarker', { 'position': new google.maps.LatLng(m.latitude, m.longitude), 'bounds':true } );
